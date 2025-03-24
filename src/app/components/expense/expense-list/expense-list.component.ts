74d8fdec83563a7f74a038fe7f234ed60b8e3511
RicardoSilva0227 <ricardo.silvaa2002@gmail.com>
2025-03-24 00:27:12 +0000

    Expenses List

--- src/app/components/expense/expense-list/expense-list.component.ts
+++ src/app/components/expense/expense-list/expense-list.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { ApiService } from '../../../services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-expense-list',
-  imports: [],
+  imports: [MatTableModule, CommonModule],
   templateUrl: './expense-list.component.html',
   styleUrl: './expense-list.component.css'
 })
-export class ExpenseListComponent {
+export class ExpenseListComponent implements OnInit {
+  expenses: any = [];
+  pageSize: number = 10;
+  pageNumber: number = 1;
 
+  constructor(private apiService: ApiService) {}
+
+
+  ngOnInit() {
+    this.fetchExpenses();
+  }
+
+  fetchExpenses() {
+    this.apiService.getAll('Expenses/GetAllExpenses', this.pageSize, this.pageNumber).subscribe(response => {
+      this.expenses = response.result;
+    });
+  }
+
+  nextPage() {
+    this.pageNumber++;
+    this.fetchExpenses();
+  }
+
+  prevPage() {
+    if (this.pageNumber > 1) {
+      this.pageNumber--;
+      this.fetchExpenses();
+    }
+  }
 }
