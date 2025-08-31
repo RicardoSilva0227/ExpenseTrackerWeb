cfd9e925bcfe041dc2060961caf7df7c0145befe
RicardoSilva0227 <ricardo.silvaa2002@gmail.com>
2025-08-31 16:49:16 +0100

    Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { ExpenseAddOrEditComponent } from './components/expense/expense-add-or-e
 import { ExpenseTypeListComponent } from './components/expenseType/expense-type-list/expense-type-list.component';
 import { ExpenseTypeAddOrEditComponent } from './components/expenseType/expense-type-add-or-edit/expense-type-add-or-edit.component';
 import { ConfigsComponent } from './components/configs/configs.component'
+import { CurrenciesListComponent } from './components/configs/Currencies/currencies-list/currencies-list.component';
+import { CurrenciesAddOrEditComponent } from './components/configs/Currencies/currencies-add-or-edit/currencies-add-or-edit.component';
 
 
 export const routes: Routes = [
@@ -22,5 +24,9 @@ export const routes: Routes = [
 
   // configs
   {path: 'Configs', component: ConfigsComponent},
+  {path: 'Configs/Currencies/List', component: CurrenciesListComponent},
+  {path: 'Configs/Currencies/AddOrEdit', component: CurrenciesAddOrEditComponent},
+  {path: 'Configs/Currencies/AddOrEdit/:id', component: CurrenciesAddOrEditComponent},
+
 ];
   
